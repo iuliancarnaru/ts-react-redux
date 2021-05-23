6639b508d8261ed887b90bb457b8cb3b9c9c6d38
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-23 22:36:06 +0100

    ts in actions

---
+++ src/state/store.ts
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+import logger from "redux-logger";
+
+const middlewares = [thunk, logger];
+
+export const store = createStore(reducers, applyMiddleware(...middlewares));
