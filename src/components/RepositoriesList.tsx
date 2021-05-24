e64278f62a9ec8d8213eda6360e467829b3d1fb4
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-24 14:34:33 +0100

    getting data in the view

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -25,6 +25,10 @@ const RepositoriesList: React.FC = () => {
         />
         <button type="submit">Search</button>
       </form>
+
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading ...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
